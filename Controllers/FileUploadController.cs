f428dc5d9e2660a01fd382a16486765e20162f0e
Eneias <medinasp@gmail.com>
2021-10-22 13:35:08 -0300

    player e upload de arquivos

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioTel.Controllers
+{
+    public class FileUploadController : Controller
+    {
+
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> FileUpload(List<IFormFile> files)
+        {
+            var size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+
+            // Get the current directory.
+            string path = Directory.GetCurrentDirectory();
+            string target = @"c:\temp";
+            Console.WriteLine("The current directory is {0}", path);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.Combine(target, formFile.FileName);
+                    filePaths.Add(filePath);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return Ok(new { files.Count, size, filePaths });
+        }
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using AudioTel.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
