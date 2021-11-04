d04bfdd358dce79da36cb8b1252610fac2f1a4a5
Eneias <medinasp@gmail.com>
2021-11-04 17:14:30 -0300

    Audio ok - banco online ok
    	deleted:    DbContext.cs
    Audio ok - banco online ok

--- Models/Gravacao.cs
+++ Models/Gravacao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -12,9 +13,12 @@ namespace AudioTel.Models
         public string Numero { get; set; }
         public string NomeDoArquivo { get; set; }
         public int? FileSize { get; set; }
+
+        [Display(Name = "Path")]
         public string FilePath { get; set; }
         public string Ramal { get; set; }
 
+        [Display(Name = "IdBC")]
         public virtual Bancocliente IdBancoClienteNavigation { get; set; }
     }
 }
